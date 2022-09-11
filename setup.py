90f2141853ba959bb391b05134a4224ab7644a54
Robert Shaw <robertshaw383@gmail.com>
2022-09-11 20:31:24 +0100

    Modernised setup.py"

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 exec(
     open('basisopt/version.py', 'r',
          encoding='utf-8').read()
@@ -7,14 +7,25 @@ exec(
 setup(name='basisopt',
       python_requires='>3.9.0',
       version=__version__,
-      packages=['basisopt',
-                'basisopt.basis',
-                'basisopt.wrappers',
-                'basisopt.viz',
-                'basisopt.opt',
-                'basisopt.testing'],
-      url='',
+      packages=find_namespace_packages(),
       license='MIT',
       author='Robert Shaw',
-      author_email='r.shaw@sheffield.ac.uk',
-      description='Automatic basis set optimization') 
+      author_email='robertshaw383@gmail.com',
+      description='Automatic basis set optimization for quantum chemistry',
+      install_requires=[
+          "colorlog",
+          "numpy",
+          "scipy",
+          "pandas",
+          "matplotlib",
+          "monty",
+          "basis_set_exchange >= 0.9",
+          "mendeleev == 0.9.0"
+      ],
+      extras_require={
+          "test": [
+              "pytest",
+              "pytest-cov",
+          ]
+      },
+) 

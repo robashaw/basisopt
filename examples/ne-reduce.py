66c8faef31c2bb69e584f4efe6e172d19e6e33c7
Grant Hill <grant.hill@sheffield.ac.uk>
2022-10-03 11:04:54 +0100

    Adds examples of multi-molecule optimisation and reduction from the paper

---
+++ examples/ne-reduce.py
@@ -0,0 +1,42 @@
+import basisopt as bo
+from basisopt.basis.atomic import AtomicBasis
+from basisopt.bse_wrapper import fetch_basis
+from basisopt.opt.reduce import ReduceStrategy
+
+bo.set_backend('psi4')
+bo.set_tmp_dir('/Users/ch1jgh/scr/')
+
+ne = AtomicBasis('Ne')
+basis_name = 'Partridge Uncontracted 3'
+start_basis = fetch_basis(basis_name, ['Ne'])
+
+#Crank the accuracy
+params = {
+    'd_convergence': "1e-8",
+    'e_convergence': "1e-8",
+    'scf_type': "pk"
+}
+
+strategy = ReduceStrategy(start_basis,
+                          eval_type='energy', method='scf',
+                          target=5e-1, shell_mins=[10, 5])
+ne.setup(method='scf', strategy=strategy, params=params, reference=(basis_name, None))
+#ne._molecule.basis = start_basis
+
+#If you want to print the guess basis
+print(bo.bse_wrapper.internal_basis_converter(start_basis, fmt='molpro'))
+
+#Compute errors and ranks
+from basisopt.testing.rank import rank_primitives
+errors, ranks = rank_primitives(ne)
+print(errors)
+print(ranks)
+
+#Actually reduce the basis
+res=ne.optimize()
+bo.write_json("neon-reduce.json", ne)
+
+from basisopt.viz.basis import plot_exponents, plot_objective
+
+
+

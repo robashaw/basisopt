6e5a2281168924aa0ec896b15232f4e4e4702b58
Robert Shaw <robertshaw383@gmail.com>
2023-02-18 18:50:02 +0000

    Regroup imports in viz_exponents

--- examples/reduce/viz_exponents.py
+++ examples/reduce/viz_exponents.py
@@ -1,13 +1,12 @@
+import matplotlib.pyplot as plt
 import basisopt as bo
 from basisopt.basis.atomic import AtomicBasis
-from basisopt.bse_wrapper import fetch_basis
-import matplotlib.pyplot as plt
 from basisopt.viz.basis import plot_exponents
 
 ne = bo.read_json("neon-reduce.json")
 
 basis_name = 'Partridge Uncontracted 3'
-start_basis = fetch_basis(basis_name, ['Ne'])
+start_basis = bo.fetch_basis(basis_name, ['Ne'])
 
 start_basis['partridge'] = start_basis['ne']
 start_basis['optimized'] = ne.get_basis()['ne']

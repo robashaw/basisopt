b3b7146ba935626271903676b396d04f91ecdb99
Grant Hill <grant.hill@sheffield.ac.uk>
2023-06-27 15:39:13 +0100

    Adds ability to use a well-tempered guess at exponents, and removes unused import in WT example

--- examples/wt_strategy.py
+++ examples/wt_strategy.py
@@ -4,7 +4,6 @@ bo.set_backend('psi4')
 bo.set_tmp_dir('tmp/')
 
 from basisopt.basis.atomic import AtomicBasis
-from basisopt.bse_wrapper import fetch_basis
 
 ne = AtomicBasis('Ne')
 ne.set_well_tempered(method='scf', accuracy=1e-4)
